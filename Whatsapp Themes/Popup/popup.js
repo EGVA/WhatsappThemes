927b31a4943f5cc2e5b318d9c49ea85be11f2dd5
EGVA <42875129+EGVA@users.noreply.github.com>
2020-06-17 10:56:13 -0300

    Add files via upload

---
+++ Whatsapp Themes/Popup/popup.js
@@ -0,0 +1,23 @@
+import {Theme} from '../Page/content.js'
+
+//new Theme('','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','','')
+
+const url = chrome.runtime.getURL('../Themes/Teste.json');
+
+fetch(url)
+    .then((response) => response.json()) //assuming file contains json
+    .then((json) => console.log(json));
+
+let theme = new Theme('a','b','c','d','e','f','e','f','g','g','g','g','g','g','g','g','g','g','g','g','g','g','g','g','g','g','g','g','g','g','g','g','g','g','g')
+
+localStorage.setItem('TEMATESTE', theme)
+
+export function GetThemeFile(themeName){
+    const url = chrome.runtime.getURL('../Themes/' + themeName + '.js');
+    let jjson;
+    fetch(url)
+        .then((response) => response.json()) //assuming file contains json
+        .then((json) => jjson = json);
+
+        return jjson;
+}
---
+++ Whatsapp Themes/module.js
@@ -0,0 +1,88 @@
+export function Theme ( 
+    unreadMessageCounterBackground_,
+    unreadMessageCounterText_,
+    mainBackgroundColour_,
+    mainBackgroundColourHover_,
+    mainBackgroundColourLighter_,
+
+    dropdownBackground_,
+    dropdownBackgroundHover_,
+
+    mainFont_,
+   
+    introBackground_,
+   
+    headerColour_,
+    headerIcons_,
+    avatarOutlines_,
+   
+    icons_,
+   
+    confgMenuBackground_,
+    confgMenuHeader_,
+
+    butterbarBackground_,
+    butterBarIcon_,
+    butterBarFont_,
+   
+    outgoingMessageBackground_,
+    outgoingMessageBackgroundDeeper_,
+    incomingMessageBackground_,
+    incomingMessageBackgroundDeeper_,
+    audioProgress_,
+    audioProgressWhilePlayed_,
+    playbackIcons_,
+    microphone_,
+    messageTime_,
+    dayIndicatorBackground_,
+    dayIndicatorText_,
+   
+    visualizedCheckmark_,
+    notVisealizedCheckmark_,
+   
+    inputFieldBackground_,
+    inputPlaceholder_,
+    inputBackground_,
+    inputBackgroundDeep_
+    )
+    {
+        this.unreadMessageCounterBackground = unreadMessageCounterBackground_
+        this.unreadMessageCounterText = unreadMessageCounterText_
+    
+        this.mainBackgroundColour = mainBackgroundColour_
+        this.mainBackgroundColourHover = mainBackgroundColourHover_
+        this.mainBackgroundColourLighter = mainBackgroundColourLighter_
+        this.unreadMessageCounterText = unreadMessageCounterText_
+    
+         this.mainFont = mainFont_
+        
+         this.introBackground = introBackground_
+        
+         this.headerColour = headerColour_
+         this.headerIcons = headerIcons_
+         this.avatarOutlines = avatarOutlines_
+        
+         this.icons = icons_
+        
+         this.confgMenuBackground = confgMenuBackground_
+         this.confgMenuHeader = confgMenuHeader_
+        
+         this.outgoingMessageBackground = outgoingMessageBackground_
+         this.outgoingMessageBackgroundDeeper = outgoingMessageBackgroundDeeper_
+         this.incomingMessageBackground = incomingMessageBackground_
+         this.incomingMessageBackgroundDeeper = incomingMessageBackgroundDeeper_
+         this.audioProgress = audioProgress_
+         this.audioProgressWhilePlayed = audioProgressWhilePlayed_
+         this.playbackIcons = playbackIcons_
+         this.messageTime = messageTime_
+         this.dayIndicatorBackground = dayIndicatorBackground_
+         this.dayIndicatorText = dayIndicatorText_
+        
+         this.visualizedCheckmark = visualizedCheckmark_
+         this.notVisealizedCheckmark = notVisealizedCheckmark_
+        
+         this.inputFieldBackground = inputFieldBackground_
+         this.inputPlaceholder = inputPlaceholder_
+         this.inputBackground = inputBackground_
+         this.inputBackgroundDeep = inputBackgroundDeep_
+}
